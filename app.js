73d2e8eb8f5513290deba920114f0fa9f0ba6cae
Ashritha Reddy Annadi <annadiashritha.an@gmail.com>
2022-10-19 17:01:14 +0530

    added file to text conversion

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync')
 const ExpressError =  require('./utils/ExpressError');
+const multer = require('multer');
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
 
 const userRoutes = require('./routes/users');
 const summeryRoutes = require('./routes/summeries');
@@ -32,6 +36,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log('hello');
+        cb(null, path.join(__dirname, '/public/uploads/'))
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '--' + file.originalname)
+    }
+})
+
+const upload = multer({storage : fileStorageEngine})
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
@@ -60,6 +76,24 @@ app.use((req, res, next) =>
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.post('/filesubmission' ,upload.single('new'), (req, res) =>
+{
+    console.log(req.file);
+    let dataBuffer = fs.readFileSync(req.file.path);
+ 
+     pdf(dataBuffer).then(function(data) {
+    const result = data.text;
+    console.log(result)
+    res.render('fileconfirmation' , {result});
+});
+})
+
+app.post('/imagesubmission' ,upload.single('img') , (req, res) =>
+{
+    console.log(req.file);
+    res.send('image upload successful');
 })
 
 app.use('/', userRoutes);
